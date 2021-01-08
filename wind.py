840cb874ee015a01eb3c30cc4f172cdb12c3eaac
Fintan Hegarty <fintan.hegarty@gmail.com>
2021-01-08 10:43:35 -0800

    updated for ignoring zero output data option

--- wind.py
+++ wind.py
@@ -26,9 +26,11 @@ def wind(sum=sum):
     if operation == 'threedegree':
         x = float(num1)
         if x > 24.45:
+            #console.log("x is", x)
             return render_template('wind.html',sum="0.\n Turbines turn off to prevent damage.")
         else:
-            sum = -0.039245646743791544 * x * x * x + 1.4820312482914098 * x * x -9.626324938316673 * x + 13.944999352467326
+            sum = -0.039245646743791544 * x**3 + 1.4820312482914098 * x**2 -9.626324938316673 * x + 13.944999352467326
+            #console.log("sum is", sum)
             return render_template('wind.html',sum=sum)
     elif operation == 'fourdegree':
         x = float(num1)
@@ -43,6 +45,13 @@ def wind(sum=sum):
             return render_template('wind.html',sum="0.\n Turbines turn off to prevent damage.")
         else:
             sum = -3.941581283061136e-06 * x * x * x * x * x * x * x + 0.0003440770101137519 * x * x * x * x * x * x -0.011534818188543508 * x * x * x * x * x + 0.1838315165430236 * x * x * x * x -1.4091946634530912 * x * x * x + 5.095745633022115 * x * x -7.582268967728165 * x + 5.985976513515224
+            return render_template('wind.html',sum=sum)
+    elif operation == 'tweaked':
+        x = float(num1)
+        if x > 24.45:
+            return render_template('wind.html',sum="0.\n Turbines turn off to prevent damage.")
+        else:
+            sum = -5.05950677021945e-06*x*x*x*x*x*x*x +0.0004416243856498899*x*x*x*x*x*x -0.01493842256542704*x*x*x*x*x +0.2441013917668856*x*x*x*x -1.9754696839749766*x*x*x +7.785955830566315*x*x -13.21083965379266*x +10.549536751342602
             return render_template('wind.html',sum=sum)
     else:
         console.log("FAIL")
